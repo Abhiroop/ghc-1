96df229e376f702de026ae80016337e70dc3e404
Abhiroop Sarkar <asiamgenius@gmail.com>
2018-07-02 23:12:22 +0100

    Enrich Xmm register with Length Width and datatype information that it can hold

--- compiler/cmm/CmmCallConv.hs
+++ compiler/cmm/CmmCallConv.hs
@@ -15,7 +15,6 @@ import PprCmm ()
 import DynFlags
 import Platform
 import Outputable
-
 -- Calculate the 'GlobalReg' or stack locations for function call
 -- parameters as used by the Cmm calling convention.
 
@@ -66,7 +65,12 @@ assignArgumentsPos dflags off conv arg_ty reps = (stk_off, assignments)
                                     | otherwise      = int
         where vec = case (w, regs) of
                       (W128, (vs, fs, ds, ls, s:ss))
-                          | passVectorInReg W128 dflags -> k (RegisterParam (XmmReg s), (vs, fs, ds, ls, ss))
+                          | passVectorInReg W128 dflags -> let elt_ty = vecElemType ty
+                                                               l      = vecLength ty
+                                                               w      = typeWidth elt_ty
+                                                            in if isFloatType elt_ty
+                                                                then k (RegisterParam (XmmReg s (Just (l, w)) (Just Float)), (vs, fs, ds, ls, ss))
+                                                                else k (RegisterParam (XmmReg s (Just (l, w)) (Just Integer)), (vs, fs, ds, ls, ss))
                       (W256, (vs, fs, ds, ls, s:ss))
                           | passVectorInReg W256 dflags -> k (RegisterParam (YmmReg s), (vs, fs, ds, ls, ss))
                       (W512, (vs, fs, ds, ls, s:ss))
@@ -211,9 +215,9 @@ realArgRegsCover :: DynFlags -> [GlobalReg]
 realArgRegsCover dflags
     | passFloatArgsInXmm dflags = map ($VGcPtr) (realVanillaRegs dflags) ++
                                   realLongRegs dflags ++
-                                  map XmmReg (realXmmRegNos dflags)
+                                  map (\x -> XmmReg x Nothing Nothing) (realXmmRegNos dflags)
     | otherwise                 = map ($VGcPtr) (realVanillaRegs dflags) ++
                                   realFloatRegs dflags ++
                                   realDoubleRegs dflags ++
                                   realLongRegs dflags ++
-                                  map XmmReg (realXmmRegNos dflags)
+                                  map (\x -> XmmReg x Nothing Nothing) (realXmmRegNos dflags)
